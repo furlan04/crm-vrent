dfabc506191bfd9d23fc26f73b01c9e58fe73490
furlangabriele <gabriele.furlan04@gmail.com>
2025-02-22 10:13:48 +0100

    fix

--- crm.vrent/crm.vrent.app/src-tauri/src/main.rs
+++ crm.vrent/crm.vrent.app/src-tauri/src/main.rs
@@ -4,24 +4,6 @@ use dotenv::dotenv;
 use std::env;
 use tauri::command;
 
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-/*#[tauri::command]
-fn get_Agenti() -> Result<(), Error> {
-    let url = "mysql://avnadmin:AVNS_qS8iPI12x5Dv_AKDmhC@crm-vrent-db-crm-vrent-db.l.aivencloud.com:18410/defaultdb?ssl-mode=REQUIRED";
-    let opts = Opts::from_url(url)?;
-    let pool = Pool::new(opts)?;
-
-    let mut conn = pool.get_conn()?;
-
-    let selected_rows = conn
-    .query_map("SELECT * FROM Agente"
-    , | Nome | {
-        Agenti { Nome }
-    });
-    
-    Ok(selected_rows);
-}*/
 #[command]
 fn get_env_var(name: &str) -> Option<String> {
     dotenv().ok(); // Carica le variabili dal file `.env`
